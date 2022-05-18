d32e3cb7487fb0dc9e275aed4079222dd02f8836
Tyler Twisle <102629991+TwisleOffical@users.noreply.github.com>
2022-05-18 17:30:50 -0400

    Update fxmanifest.lua
    Got Rid of the config.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,10 +5,6 @@ author 'Twisle'
 description 'A Script That Lets You Change The Drivetrain'
 version '1.0.0'
 
--- What to run
-shared_scripts { 
-    'config.lua'
-}
 client_scripts {
     'cl_drivetrain.lua',
 }
